8280d3a5b5683a1db40a7c909267285e8f722332
ProRedCat <reilly.oldham@gmail.com>
2024-08-31 16:27:58 +1200

    Fix apostrophe escape

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <div className="flex justify-center items-center h-lvh bg-primary-cream">
             <div className="w-full max-w-md p-6 border border-4 border-primary-navy-dark rounded-lg">
-                <h2 className="text-primary-navy-dark font-sans text-2xl mb-4">Hi! I'm Reilly.</h2>
+                <h2 className="text-primary-navy-dark font-sans text-2xl mb-4">Hi! I&apos;m Reilly.</h2>
                 <p className="italic text-primary-navy-dark text-lg mb-6">
                     This site is under construction.
                 </p>
