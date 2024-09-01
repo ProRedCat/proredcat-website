b2eef88927e10579835e8643e8367b053ec90fcd
Reilly Oldham <oldhamreil@myvuw.ac.nz>
2024-09-01 22:47:21 +1200

    Header refinements

--- components/header.tsx
+++ components/header.tsx
@@ -1,37 +1,37 @@
-import Link from "next/link";
-import {Icons} from "@/components/icons";
-import {siteConfig} from "@/config/site";
-import {PageNav} from "@/components/page-nav";
+    import Link from "next/link";
+    import {Icons} from "@/components/icons";
+    import {siteConfig} from "@/config/site";
+    import {PageNav} from "@/components/page-nav";
 
-export function Header() {
-    return (
-        <>
-            <header className="z-10 sticky top-0 w-full">
-                <div className="container mx-auto flex h-14 max-w-screen-2xl items-center justify-between">
-                    <div className="flex-1">
-                        <PageNav/>
+    export function Header() {
+        return (
+            <>
+                <header className="z-10 sticky top-0 w-full">
+                    <div className="container mx-auto flex h-16 max-w-screen-2xl items-center justify-between">
+                        <div className="flex-1">
+                            <PageNav/>
+                        </div>
+                        <nav className="flex items-center space-x-2">
+                            <Link
+                                href={siteConfig.links.github}
+                                className="pb-2 pt-2"
+                            >
+                                <Icons.GitHub className="h-8 w-8"/>
+                                <span className="sr-only">GitHub</span>
+                            </Link>
+                            <Link
+                                href={siteConfig.links.linkedin}
+                                className="pb-2 pt-2"
+                            >
+                                <Icons.LinkedIn className="h-9 w-9"/>
+                                <span className="sr-only">LinkedIn</span>
+                            </Link>
+                        </nav>
                     </div>
-                    <nav className="flex items-center space-x-2">
-                        <Link
-                            href={siteConfig.links.github}
-                            className="pb-2 pt-2"
-                        >
-                            <Icons.GitHub className="h-10 w-10"/>
-                            <span className="sr-only">GitHub</span>
-                        </Link>
-                        <Link
-                            href={siteConfig.links.linkedin}
-                            className="pb-2 pt-2"
-                        >
-                            <Icons.LinkedIn className="h-10 w-10"/>
-                            <span className="sr-only">LinkedIn</span>
-                        </Link>
-                    </nav>
+                </header>
+                <div className="sticky top-14 w-full">
+                    <div className="w-full h-1 bg-primary-navy-dark rounded-full"></div>
                 </div>
-            </header>
-            <div className="sticky top-14 w-full">
-                <div className="w-full h-2 bg-primary-navy-dark rounded-full"></div>
-            </div>
-        </>
-    );
-}
+            </>
+        );
+    }
