6f628f95baca3cabf1023e5cbb641ef27fff1952
Reilly Oldham <oldhamreil@myvuw.ac.nz>
2024-09-11 15:56:39 +1200

    Change prose styling for blog to match colour scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,20 +15,37 @@ const config: Config = {
       },
       colors: {
         'primary-cream': '#FFFDE7',
-
         'primary-navy-dark': '#001A4D',
-
         'primary-green-light': '#D8FFE7',
         'primary-green-dark': '#003E19',
-
         'primary-blue-light': '#D8FFFD',
         'primary-blue-dark': '#003D39',
-
         'red-secondary': '#FF0000',
         'orange-secondary': '#FF0000',
-      }
+      },
+      // @ts-ignore
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.primary-navy-dark'),
+            a: {
+              color: theme('colors.primary-navy-dark'),
+            },
+            h1: { color: theme('colors.primary-navy-dark') },
+            h2: { color: theme('colors.primary-navy-dark') },
+            h3: { color: theme('colors.primary-navy-dark') },
+            h4: { color: theme('colors.primary-navy-dark') },
+            strong: { color: theme('colors.primary-navy-dark') },
+            blockquote: {
+              color: theme('colors.primary-green-dark'),
+              borderLeftColor: theme('colors.primary-green-light'),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
+
 export default config;
