44ee257d03f229040b79678abe9d938d85168849
Reilly Oldham <reilly.oldham@gmail.com>
2024-10-29 20:29:35 +1300

    Fixups before announcement

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,9 @@
 export default function AboutPage() {
-    return "About Page";
+    return <div className="flex pt-16 justify-center items-center bg-primary-cream">
+        <div className="w-full max-w-md p-6 border-4 border-primary-navy-dark rounded-lg">
+            <p className="italic text-primary-navy-dark text-lg">
+                This page is also under construction
+            </p>
+        </div>
+    </div>
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,8 +3,17 @@ import HumanMotionDiffusion from "@/components/projects/human-motion";
 
 export default function ProjectsPage() {
     return (
-        <div className="mx-auto pt-4">
+        <div className="mx-auto pt-16">
             <h1 className="text-2xl text-center">Main Projects</h1>
+
+            <div className="flex pt-4 justify-center items-center bg-primary-cream">
+                <div className="w-full max-w-md p-6 border-4 border-primary-navy-dark rounded-lg">
+                    <p className="italic text-primary-navy-dark text-lg">
+                        This page is under partial construction
+                    </p>
+                </div>
+            </div>
+
             <AlexaTransportProject/>
 
             {/*<hr className="mt-4 w-2/3 mx-auto"/>*/}
