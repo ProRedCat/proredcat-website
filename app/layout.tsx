bcccce349e0e56cb69a5213958216ab662ab4ff1
Reilly Oldham <43058406+ProRedCat@users.noreply.github.com>
2025-03-20 23:52:49 +1300

    Remove site verification - done through search console

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,9 +52,6 @@ export const metadata: Metadata = {
             'max-snippet': -1,
         },
     },
-    verification: {
-        google: "your-google-site-verification",
-    },
     alternates: {
         canonical: "https://www.proredcat.xyz",
     },
