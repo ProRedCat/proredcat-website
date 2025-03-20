882fa951764f19127ec0916eb6d32acbb61c9445
Reilly Oldham <43058406+ProRedCat@users.noreply.github.com>
2025-03-21 00:17:28 +1300

    Mobile navigation

--- components/header.tsx
+++ components/header.tsx
@@ -1,37 +1,41 @@
-    import Link from "next/link";
-    import {Icons} from "@/components/icons";
-    import {siteConfig} from "@/config/site";
-    import {PageNav} from "@/components/page-nav";
+import Link from "next/link";
+import {Icons} from "@/components/icons";
+import {siteConfig} from "@/config/site";
+import {PageNav} from "@/components/page-nav";
 
-    export function Header() {
-        return (
-            <>
-                <header className="z-10 sticky top-0 w-full bg-primary-cream">
-                    <div className="container mx-auto flex h-16 max-w-screen-2xl items-center justify-between">
-                        <div className="flex-1">
-                            <PageNav/>
-                        </div>
-                        <nav className="flex items-center space-x-2">
-                            <Link
-                                href={siteConfig.links.github}
-                                className="pb-2 pt-2"
-                            >
-                                <Icons.GitHub className="h-8 w-8"/>
-                                <span className="sr-only">GitHub</span>
-                            </Link>
-                            <Link
-                                href={siteConfig.links.linkedin}
-                                className="pb-2 pt-2"
-                            >
-                                <Icons.LinkedIn className="h-9 w-9"/>
-                                <span className="sr-only">LinkedIn</span>
-                            </Link>
-                        </nav>
+export function Header() {
+    return (
+        <>
+            <header className="z-40 sticky top-0 w-full bg-primary-cream">
+                <div className="container mx-auto flex h-16 max-w-screen-2xl items-center justify-between px-4">
+                    <div className="flex-1">
+                        <PageNav/>
                     </div>
+                    <nav className="flex items-center space-x-2">
+                        <Link
+                            href={siteConfig.links.github}
+                            className="pb-2 pt-2"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Icons.GitHub className="h-8 w-8"/>
+                            <span className="sr-only">GitHub</span>
+                        </Link>
+                        <Link
+                            href={siteConfig.links.linkedin}
+                            className="pb-2 pt-2"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Icons.LinkedIn className="h-9 w-9"/>
+                            <span className="sr-only">LinkedIn</span>
+                        </Link>
+                    </nav>
+                </div>
                 <div className="sticky top-14 w-full">
                     <div className="w-full h-1 bg-primary-navy-dark rounded-full"></div>
                 </div>
-                </header>
-            </>
-        );
-    }
+            </header>
+        </>
+    );
+}
